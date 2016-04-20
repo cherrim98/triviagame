a1fbeefc02bbb351478e62990f452df93323ee38
Aaron <cherrim98@gmail.com>
2016-04-20 09:17:36 -0400

    Added files via upload

--- triviagame/scripts.js
+++ triviagame/scripts.js
@@ -9,6 +9,25 @@
 $(document).ready(function() {
 
   var playerName = '';
+  var playerAnswer1 = '';
+  var answer1 = 'frisk'
+  var correctCounter = 0;
+
+  // Hide Player Name
+  $('#dashboard').hide();
+  $('.question').hide();
+  $('.correct-msg').hide();
+  $('.incorrect-msg').hide();
+
+  // Allow user to type name immediately on page load
+  $('#player-name').focus();
+
+    // When user submits name, welcome screen disappears
+  function removeWelcomeScreen() {
+    $('#welcome-screen').detach();
+    $('#dashboard').show().addClass('animated fadeIn');
+    $('#question1').show().addClass('animated fadeIn').focus();
+  };
 
   // Store player name
   $('#name-button').on('click', function(e) {
@@ -16,6 +35,36 @@ $(document).ready(function() {
     playerName = $('#player-name').val();
     console.log(playerName);
     $('#show-player-name').text(playerName);
-  })
+    $('#welcome-screen').addClass('animated fadeOut');
+    setTimeout(removeWelcomeScreen, 1000);
+  });
 
-}) // ready function ends
+  // Store answer 1
+  $('#question1-btn').on('click', function(e) {
+    e.preventDefault(); // prevents form from submitting to a database
+    playerAnswer1 = $('#input-question1').val().trim().toLowerCase(); // This assigns the user's input to the variable playerAnswer1
+    console.log('The players answer for question 1 is ' + playerAnswer1);
+    // showPlayerAnswer1(); // This first showPlayerAnswer1 function
+    answerCheck1();
+  });
+
+  // Show answers
+  function showPlayerAnswer1() {
+    $('#show-player-answer1').text(playerAnswer1);
+    $('#correct-answer1').text(answer1)
+  };
+
+  function answerCheck1() {
+    if(playerAnswer1 === answer1) {
+      console.log("Player's answer is correct!");
+      $('#correct-msg1').show().addClass('animated fadeIn');
+      correctCounter++;
+      $('#score').text(correctCounter);
+    }
+    else {
+      console.log("Player's answer is incorrect!");
+      $('#incorrect-msg1').show().addClass('animated fadeIn');
+    }
+  };
+
+}); // ready function ends
